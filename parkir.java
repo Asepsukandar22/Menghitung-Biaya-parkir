9e6dc4143c63661bf48fbb5bdf3e07a01acd9c00
Asepsukandar22 <asep.sukandar_ti23@nusaputra.ac.id>
2023-10-02 20:25:29 +0700

    Tugas Sistem Parkir

---
+++ parkir.java
@@ -0,0 +1,14 @@
+public class parkir{
+      public static void main(String[]args){
+       int jam_masuk = 7;
+       int jam_keluar = 12;
+       int biayaPerjam = 2000;
+       int total_jam_parkir = jam_keluar - jam_masuk; //Perhitungan Jam Parkir
+       int Hasil = total_jam_parkir * biayaPerjam;    //Perhitungan Total Biaya Bayar
+       //By. Asep Sukandar
+       System.out.println("SELAMAT DATANG DI SISTEM PARKIR");
+       System.out.println("==============================");
+       System.out.println("Biaya Yang Harus Di keluarkan Sebesar RP."+Hasil); //Cetak Hasil
+    } 
+    Asepsukandar23
+}
